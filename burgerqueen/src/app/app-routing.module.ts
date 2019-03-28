8b3b0fee0b7cea5a6c3dd2b135003e5709de5781
DaniAntu <d.asenjo.c@gmail.com>
2019-03-28 16:20:25 -0300

    Se configura routing

--- burgerqueen/src/app/app-routing.module.ts
+++ burgerqueen/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { WaiterComponent } from './waiter/waiter.component';
+import { ChefComponent } from './chef/chef.component';
+import { StatusComponent } from './status/status.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'waiter', component: WaiterComponent },
+  { path: 'chef', component: ChefComponent },
+  { path: 'status', component: StatusComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
